ee47ef79ca3f6c77e9fa32089b2ae03ff1dc75fc
ZairiJalal <zairi.jalal98@gmail.com>
2021-03-16 10:23:40 +0100

    Produit

--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -2,18 +2,36 @@ import React from 'react';
 import { Card, CardMedia, CardContent, CardActions, Typography, IconButton } from '@material-ui/core';
 import { AddShoppingCart } from '@material-ui/icons';
 
-import useStyles from './styles';
 
 const Product = ({ product, onAddToCart }) => {
-  const classes = useStyles();
+
+  
+  const classes = {
+    root: {
+      // maxWidth: 345, original width style
+      maxWidth: '100%',
+    },
+    media: {
+      height: 0,
+      paddingTop: '56.25%', // 16:9
+    },
+    cardActions: {
+      display: 'flex',
+      justifyContent: 'flex-end',
+    },
+    cardContent: {
+      display: 'flex',
+      justifyContent: 'space-between',
+    }
+  }
 
   const handleAddToCart = () => onAddToCart(product.id, 1);
 
   return (
-    <Card className={classes.root}>
-      <CardMedia className={classes.media} image={product.media.source} title={product.name} />
+    <Card style={classes.root}>
+      <CardMedia style={classes.media} image={product.media.source} title={product.name} />
       <CardContent>
-        <div className={classes.cardContent}>
+        <div style={classes.cardContent}>
           <Typography gutterBottom variant="h5" component="h2">
             {product.name}
           </Typography>
@@ -23,7 +41,7 @@ const Product = ({ product, onAddToCart }) => {
         </div>
         <Typography dangerouslySetInnerHTML={{ __html: product.description }} variant="body2" color="textSecondary" component="p" />
       </CardContent>
-      <CardActions disableSpacing className={classes.cardActions}>
+      <CardActions disableSpacing style={classes.cardActions}>
         <IconButton aria-label="Add to Cart" onClick={handleAddToCart}>
           <AddShoppingCart />
         </IconButton>

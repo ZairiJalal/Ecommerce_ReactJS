b1f25aae3d9f3df0b611ec7c3be643fc08430d89
ZairiJalal <zairi.jalal98@gmail.com>
2021-03-17 23:50:03 +0100

    cart item

--- src/components/Cart/CartItem/CartItem.jsx
+++ src/components/Cart/CartItem/CartItem.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import { Typography, Button, Card, CardActions, CardContent, CardMedia } from '@material-ui/core';
 
-import useStyles from './styles';
 
 const CartItem = ({ item, onUpdateCartQty, onRemoveFromCart }) => {
-  const classes = useStyles();
+  const classes = {
+    media: {
+      height: 260,
+    },
+    cardContent: {
+      display: 'flex',
+      justifyContent: 'space-between',
+    },
+    buttons: {
+      display: 'flex',
+      alignItems: 'center',
+    }
+  };
 
   const handleUpdateCartQty = (lineItemId, newQuantity) => onUpdateCartQty(lineItemId, newQuantity);
 
@@ -12,13 +23,13 @@ const CartItem = ({ item, onUpdateCartQty, onRemoveFromCart }) => {
 
   return (
     <Card className="cart-item">
-      <CardMedia image={item.media.source} alt={item.name} className={classes.media} />
-      <CardContent className={classes.cardContent}>
+      <CardMedia image={item.media.source} alt={item.name} style={classes.media} />
+      <CardContent style={classes.cardContent}>
         <Typography variant="h4">{item.name}</Typography>
         <Typography variant="h5">{item.line_total.formatted_with_symbol}</Typography>
       </CardContent>
-      <CardActions className={classes.cardActions}>
-        <div className={classes.buttons}>
+      <CardActions style={classes.cardActions}>
+        <div style={classes.buttons}>
           <Button type="button" size="small" onClick={() => handleUpdateCartQty(item.id, item.quantity - 1)}>-</Button>
           <Typography>&nbsp;{item.quantity}&nbsp;</Typography>
           <Button type="button" size="small" onClick={() => handleUpdateCartQty(item.id, item.quantity + 1)}>+</Button>
